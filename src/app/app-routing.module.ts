15d71b05d89060cc75f2f78cea4bab0647d2d98d
sis0k0 <stanimira.vlaeva@gmail.com>
2019-01-27 13:34:07 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'lazy' },
+  { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
